84ebca7988e4c6d209464064ef2da73208e15011
orobosa <corneliusorobosa22@gmail.com>
2025-02-05 19:10:44 +0000

    trial

--- index.py
+++ index.py
@@ -1,5 +1,5 @@
 from flask import Flask, jsonify, request
-from utils import is_prime, sum_digit, get_fun_fact, get_number_properties, is_perfect
+#from utils import is_prime, sum_digit, get_fun_fact, get_number_properties, is_perfect
 
 app = Flask(__name__)
 
@@ -7,22 +7,22 @@ app = Flask(__name__)
 def home():
     return "Flask server is running on Vercel!"
 
-@app.route("/api/classify-number")
-def classify_num():
-    number = request.args.get('number')
+# #@app.route("/api/classify-number")
+# def classify_num():
+#     number = request.args.get('number')
 
-    if not number or not number.isdigit():
-        return jsonify({"error": "Invalid number"}), 400
+#     if not number or not number.isdigit():
+#         return jsonify({"error": "Invalid number"}), 400
 
-    number = int(number)  
+#     number = int(number)  
 
-    return jsonify({
-        "is_prime": is_prime(number),
-        "digit_sum": sum_digit(str(number)),
-        "fun_fact": get_fun_fact(number),
-        "properties": get_number_properties(str(number)),
-        "is_perfect": is_perfect(number)
-    })
+#     return jsonify({
+#         "is_prime": is_prime(number),
+#         "digit_sum": sum_digit(str(number)),
+#         "fun_fact": get_fun_fact(number),
+#         "properties": get_number_properties(str(number)),
+#         "is_perfect": is_perfect(number)
+#     })
 
 
 if __name__ == "__main__":
